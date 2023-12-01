9e34ad4804d168be7388784e6fdf4b5ddbe1634a
oberger <ole.k.berger@t-online.de>
2023-12-01 11:35:23 +0100

    Extend template

--- template/solve.cpp
+++ template/solve.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include "utils.h"
 
-int main()
-{
+void solvePart1() {
   // TODO
+}
+
+void solvePart2() {
+  // TODO
+}
+
+int main() {
+  measureTime(&solvePart1, "Part 1");
+  measureTime(&solvePart2, "Part 2");
   std::cout << "Done.\n";
   return 0;
 }
---
+++ template/utils.h
@@ -0,0 +1,32 @@
+#include <chrono>
+#include <iomanip>
+#include <iostream>
+
+template <typename Func> void measureTime(Func func, const std::string &name) {
+  std::cout << "Running " << name << "...\n";
+  const auto start = std::chrono::steady_clock::now();
+  func();
+  const auto end = std::chrono::steady_clock::now();
+  const auto diff = end - start;
+  // Print duration in readable form.
+  std::cout << name << " took ";
+  if (std::chrono::duration_cast<std::chrono::milliseconds>(diff).count() >=
+      10000) {
+    const auto s =
+        std::chrono::duration_cast<std::chrono::milliseconds>(diff).count() /
+        1000.0;
+    std::cout << std::setprecision(6) << s << " s\n";
+  }
+  if (std::chrono::duration_cast<std::chrono::milliseconds>(diff).count() <
+      10) {
+    const auto ms =
+        std::chrono::duration_cast<std::chrono::microseconds>(diff).count() /
+        1000.0;
+    std::cout << std::setprecision(6) << ms << " ms\n";
+
+  } else {
+    std::cout
+        << std::chrono::duration_cast<std::chrono::milliseconds>(diff).count()
+        << " ms\n";
+  }
+}
